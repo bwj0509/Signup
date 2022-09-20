1af787723c00a6862e23c5fd229dcf2a1b6ea7c4
[woojin] <bwj59@naver.com>
2022-09-20 12:53:20 +0900

    pr

--- src/Componets/Signup.js
+++ src/Componets/Signup.js
@@ -8,7 +8,6 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import { fontWeight } from '@mui/system';
 
 
 function Signup() {
